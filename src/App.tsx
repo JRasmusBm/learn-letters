1684a3874920ff52af353fd933dda3864717c85a
jrasmusbm <JRasmusBm@gmail.com>
2022-11-06 15:16:30 +0100

    feat(letters): build MVP

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,75 @@
 import * as React from "react";
 import "./App.css";
 
+const letters = "ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvxyz0123456789";
+function randomLetter() {
+  return letters.charAt(Math.floor(Math.random() * letters.length));
+}
+
 function useLetter() {
-  const [letter, setLetter] = React.useState("A");
+  const initalLetter = React.useMemo(() => randomLetter, []);
+  const [letter, setLetter] = React.useState(initalLetter);
 
   return {
     letter,
-    updateLetter() {
-      setLetter("B");
+    attempt(guess: string) {
+      if (guess?.toLowerCase() === letter?.toLowerCase()) {
+        setLetter(randomLetter());
+        return true;
+      }
+      return false;
+    },
+  };
+}
+
+type Result = null | "right" | "wrong";
+function useResult() {
+  const [result, setResult] = React.useState<Result>(null);
+
+  return {
+    result,
+    setResult(result: Exclude<Result, null>) {
+      setResult(result);
+      setTimeout(() => {
+        setResult(null);
+      }, 1000);
     },
   };
 }
 
 function App() {
-  const { letter } = useLetter();
+  const { letter, attempt } = useLetter();
+  const { result, setResult } = useResult();
+  
 
   return (
     <main>
-      <output>{letter}</output>
+      <input
+      style={{
+        backgroundColor: result === "right" ? "darkgreen" : result === "wrong" ? "darkred" : "inherit"
+      }}
+        value={letter}
+        onKeyDown={(e) => {
+          e.preventDefault();
+
+          if (result) {
+            return
+            
+          }
+
+          
+
+          if (attempt(e.key)) {
+            setResult("right")
+            return;
+          }
+
+          setResult("wrong")
+        }}
+        autoFocus
+      />
     </main>
   );
 }
 
 export default App;
-
