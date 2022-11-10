0ee57c38a33284e0b68f20280f9b58fc79d81cd6
jrasmusbm <JRasmusBm@gmail.com>
2022-11-10 18:34:06 +0100

    feat(letters): support other modes

--- src/hooks/letter.tsx
+++ src/hooks/letter.tsx
@@ -3,6 +3,15 @@ import * as React from "react";
 export const supportedLetters =
   "ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvxyz0123456789";
 
+const state = { i: 0 };
+const words = localStorage.getItem("LEARN_LETTERS_WORDS") || [
+  "Mary",
+  "had",
+  "a",
+  "little",
+  "lamb",
+];
+
 const wordGenerators = {
   randomLetters() {
     return Array.from({ length: 5 }, () =>
@@ -10,6 +19,13 @@ const wordGenerators = {
         Math.floor(Math.random() * supportedLetters.length)
       )
     ).join("");
+  },
+  loopWords() {
+    state.i += 1;
+    return words[state.i % words.length];
+  },
+  loopLetters() {
+    return supportedLetters;
   },
 };
 
